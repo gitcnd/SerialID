9816d2d6afa17b337c42f5538668d2e94f6daf2b
Chris Drake <cnd@geek.net.au>
2018-12-22 21:15:17 +1000

    Fixed #ifdef

--- SerialID.h
+++ SerialID.h
@@ -3,20 +3,20 @@
   (C) Copyright 2018 (GNU GENERAL PUBLIC LICENSE, Version 3) - Chris Drake <cdrake@cpan.org>
 */
 
-#ifndef MorseLED_h
-#define MorseLED_h
+#ifndef SerialID_h
+#define SerialID_h
 
 #define SerialIDset(VER) const char VERSION[] PROGMEM=VER;
 #ifndef TEENSYDUINO
 #define SerialIDshow(BAUD) Serial.begin(BAUD); \
   while((!Serial)&&(micros()<2000000)); \
-  Serial.println((__FlashStringHelper*)VERSION);
+  Serial.println((__FlashStringHelper*)VERSION);Serial.flush();
 #else
 #define SerialIDshow(BAUD) Serial.begin(BAUD); \
   while((!Serial)&&(micros()<2000000)); \
   Serial.println((__FlashStringHelper*)VERSION); \
   delay(5000); \
-  Serial.println((__FlashStringHelper*)VERSION); // wait 5s for PC to re-connect serial
+  Serial.println((__FlashStringHelper*)VERSION);Serial.flush(); // wait 5s for PC to re-connect serial
 #endif
 
 #endif
